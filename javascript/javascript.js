20c93fce448d2d42de3fec964f7d93cdc6c1317e
Rahulrk600 <rahulbca600@gmail.com>
2024-02-14 19:10:35 +0530

    first commit

---
+++ javascript/javascript.js
@@ -0,0 +1,17 @@
+document.querySelector('.cross').style.display ='none';
+document.querySelector('.hamburger').addEventListener("click", ()=>{
+     document.querySelector('.sidebar').classList.toggle('sidebargo');
+     if(document.querySelector('.sidebar').classList.contains('sidebargo')){
+        setTimeout(() =>{
+            document.querySelector('.ham').style.display ='inline'
+        },300)
+        document.querySelector('.cross').style.display= 'none'
+     }
+     else{
+            document.querySelector('.ham').style.display ='none'
+        setTimeout(() =>{
+            document.querySelector('.cross').style.display ='inline'
+        },300)
+        
+     }
+})
